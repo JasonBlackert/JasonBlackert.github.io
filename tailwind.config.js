43c0ddcf141d9529eb85f9c76b44e26f947ba477
Jason Blackert <jason.blackert@gmail.com>
2024-04-23 23:10:35 -0500

    Update 1.1  - Split personal and engineering projects in two  - added quote under biography  - reduced shadow 20px -> 5px  - increase footer compatibility for mobile devices      - reduce text & padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,7 @@ module.exports = {
     extend: {
       boxShadow: {
         neon: "0 0 5px theme('colors.indigo.200'), 0 0 20px theme('colors.indigo.700')",
-        low: "0 0 30px theme('colors.indigo.400')",
-        high: "0 0 30px theme('colors.indigo.700')"
+        low: "0 0 5px theme('colors.indigo.400')",
       }
     },
   },
