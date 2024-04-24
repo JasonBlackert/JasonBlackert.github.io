2ee4106a5ed0664915f27e74824e642fedecf93b
Jason Blackert <jason.blackert@gmail.com>
2024-04-23 21:34:57 -0500

    Update shadows to:  - include testimonials  - match color of header/footer  - expand pixel from 20px -> 30px

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        neon: "0 0 5px theme('colors.indigo.200'), 0 0 20px theme('colors.indigo.700')"
+        neon: "0 0 5px theme('colors.indigo.200'), 0 0 20px theme('colors.indigo.700')",
+        low: "0 0 30px theme('colors.indigo.400')",
+        high: "0 0 30px theme('colors.indigo.700')"
       }
     },
   },
